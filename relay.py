45f40e6db3d64bedf85aac256b2e267f8f606d18
a0920069082b <a0920069082b@gmail.com>
2020-02-21 14:02:15 +0800

    ApiFlaskDebug

--- relay.py
+++ relay.py
@@ -1,4 +1,4 @@
-from flask import FlaskAPI, request
+from flask import Flask, request
 from flask_cors import CORS
 import json
 import time
@@ -41,4 +41,4 @@ def index():
 
 if __name__ == '__main__':
     app.debug = False
-    app.run(host='0.0.0.0', port=5000 ,debug=True) 
+    app.run(host='0.0.0.0', port=5000 ,debug=False)
