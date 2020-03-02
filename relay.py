c63025622b4f5b1fda6ff1dfdadf2bf9c579c76b
a0920069082b <a0920069082b@gmail.com>
2020-03-02 08:15:18 +0800

    RelayOnApi與RelayOffApi開發

--- relay.py
+++ relay.py
@@ -8,8 +8,8 @@ import RPi.GPIO as GPIO
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/api/RelayControll", methods=["GET"])
-def index():
+@app.route("/api/RelayControll/On", methods=["GET"])
+def RelayOn():
     with open('config.json') as json_file:
         config = json.load(json_file)
         machine_id = config["machine_id"]
@@ -20,9 +20,37 @@ def index():
         DateNow = datetime.datetime.now()
         try:
             GPIO.output(relay_pin,  GPIO.HIGH)   # Turn motor on
-            time.sleep(relay_switch_time)
+            msg = "Call Relay On success"
+        except Exception as e:
+            msg = e
+
+        if  (request.headers.getlist("X-Forwarded-For")):
+           ip = request.headers.getlist("X-Forwarded-For")[0]
+        else:
+            ip = request.remote_addr
+
+        f = open('log.txt','a')
+        log = 'machine: {0},message:{1},ip:{2},time:{3}\r\n'.format(machine_id,msg,ip,DateNow)
+        f.write(log )
+
+        return  json.dumps({
+                "machine": machine_id,
+                "msg": msg
+        })
+
+@app.route("/api/RelayControll/Off", methods=["GET"])
+def RelayOff():
+    with open('config.json') as json_file:
+        config = json.load(json_file)
+        machine_id = config["machine_id"]
+        relay_pin = config['RELAY_PIN']
+        relay_switch_time = config["RELAY_SWITCH_TIME"]
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(relay_pin, GPIO.OUT)
+        DateNow = datetime.datetime.now()
+        try:
             GPIO.output(relay_pin, GPIO.LOW)  # Turn motor
-            msg = "Call Relay success"
+            msg = "Call Relay Off success"
         except Exception as e:
             msg = e
 
